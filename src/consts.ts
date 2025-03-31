cf711bf2a1af8462c96ff36d06f9b0609100691d
HelloPages <201108396+HelloPages@users.noreply.github.com>
2025-03-31 16:54:29 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -17,6 +17,10 @@ export const NAV_LINKS: SocialLink[] = [
     label: 'blog',
   },
   {
+    href: '/bookmark',
+    label: 'bookmark',
+  },
+  {
     href: '/authors',
     label: 'authors',
   },
